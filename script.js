fd7de7cab0bdaf14e6f713ddff7c9ab2651f0cb9
Ben Miller <benjaminmiller1@students.berkeley.net>
2017-02-10 10:47:11 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,16 @@
  * Created by h205p3 on 2/8/17.
  */
 function gitTest() {
-
-    console.log("this should appear in github");
-
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += '?' + $.param({
+            'api-key': "b6cdaafb43c84cf0bd83533bcbe93e4c"
+        });
+    $.ajax({
+        url: url,
+        method: 'GET',
+    }).done(function(result) {
+        console.log(result);
+    }).fail(function(err) {
+        throw err;
+    });
 }
